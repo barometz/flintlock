f848c122574124d703ccae5df775e588cc3bc421
barometz <dominic@baudvine.net>
2013-03-18 16:46:33 +0100

    Version bump etc for release

--- flintlock/Properties/AssemblyInfo.cs
+++ flintlock/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("flintlock")]
+[assembly: AssemblyTitle("Flintlock")]
 [assembly: AssemblyDescription("A Windows application to interact with Pebble smartwatches")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("flintlock")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Flintlock")]
+[assembly: AssemblyCopyright("Copyright © Dominic van Berkel 2013, see LICENSE.txt for details.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.5.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
